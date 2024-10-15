4117300e91e4c0f0278291fa37b51fae90928039
pgallardob <100787476+pgallardob@users.noreply.github.com>
2024-10-15 20:33:08 -0300

    Add files via upload
    Se agrega carpeta js con archivos empresa.js, importacion.js index.js

--- EjercicioGrupal4.2/assets/css/js/empresa.js
+++ EjercicioGrupal4.2/assets/css/js/empresa.js
@@ -1,35 +1,28 @@
-class Empresa {
-  constructor(idRegistro, nombre, rut) {
-    this.idRegistro = idRegistro;
-    this.nombre = nombre;
-    this.rut = rut;
-    this.importaciones = [];
-    this.totalImportaciones = 0;
-  }
-  getIdRegistro() {
-    return this.idRegistro;
-  }
-  setIdRegistro(idRegistro) {
-    this.idRegistro = idRegistro;
-  }
-  getNombre() {
-    return this.nombre;
-  }
-  setNombre(nombre) {
-    this.nombre = nombre;
-  }
-  getRut() {
-    return this.rut;
-  }
-  setRut(rut) {
-    this.rut = rut;
-  }
-  agregarImportacion(importacion) {
-    this.importaciones.push(importacion);
-    this.totalImportaciones += importacion.getPrecioTotal();
-  }
-  getTotalImportaciones() {
-    return this.totalImportaciones;
-  }
+export default class Empresa {
+    static contadorId = 0;
+
+    constructor(nombre, rut) {
+        this.idRegistro = Empresa.contadorId++;
+        this.nombre = nombre;
+        this.rut = rut;
+        this.importaciones = [];
+    }
+
+    getIdRegistro() { return this.idRegistro }
+    setIdRegistro(idRegistro) { this.idRegistro = idRegistro }
+
+    getNombre() { return this.nombre }
+    setNombre(nombre) { this.nombre = nombre }
+
+    getRut() { return this.rut }
+    setRut(rut) { this.rut = rut }
+
+    registrarImportacion(importacion) {
+        this.importaciones.push(importacion);
+        // console.log(importacion)
+    }
+
+    obtenerTotalImportaciones() {
+        return this.importaciones.reduce((total, importacion) => total + importacion.obtenerCostoTotal(), 0)
+    }
 }
-export default Empresa;
